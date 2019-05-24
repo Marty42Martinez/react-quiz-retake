c8805af3d84d09252f1141cd9a170039c2bd739d
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-24 10:29:50 -0700

    Final styling, good shiz

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Meme from '../containers/Meme';
+import styles from './App.css';
 
 export default function App() {
 
   return (
     <>
-      <h1>Hello Developer</h1>
-      <h2>Lettuce make some spicy Memes</h2>
+      <header className={styles.Header}>
+        <h1>Hello Developer</h1>
+        <h2>Lettuce make some spicy Memes</h2>
+      </header>
       <Meme />
     </>
   );
